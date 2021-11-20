31f7f698a772dcd101d742f1b5bc0acf2362bdc8
dadelosreyes <DELOSREYES.DAVE@UE.EDU.PH>
2021-11-21 01:47:48 +0800

    made plain log in form

--- index.php
+++ index.php
@@ -4,9 +4,48 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Log in</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link type="text/css" href="/assets/css/argon.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/7ae2cf8851.js" crossorigin="anonymous"></script>
 </head>
 <body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        <h3>Log in</h3>
+                    </div>
+                    <div class="card-body">
+                        <form action="#" method="post">
+
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="username">
+                                <i class="fas fa-user"></i>
+                            </span>
+                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="username">
+                        </div>
+                        
+                        <div class="input-group mb-3">
+                            <span class="input-group-text" id="password">
+                                <i class="fas fa-lock"></i>
+                            </span>
+                            <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password">
+                        </div>
+                        <div class="text-center d-grid gap-2 mt-4 mb-4">
+                            <button type="submit" name="" class="btn btn-primary mb-4">Sign in</button>
+                            <p>Not registered yet? <a href="#">Register</a></p>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
     
 </body>
 </html>
