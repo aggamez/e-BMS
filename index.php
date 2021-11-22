3af2217848cfb8309107559e3bd1c7aee4d661ca
jdpore <pore.johndexter@ue.edu.ph>
2021-11-22 23:02:18 +0800

    updated login

--- index.php
+++ index.php
@@ -1,51 +1,94 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Log in</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link type="text/css" href="/assets/css/argon.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/7ae2cf8851.js" crossorigin="anonymous"></script>
-</head>
-<body>
-    <div class="container mt-5">
-        <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-header">
-                        <h3>Log in</h3>
-                    </div>
-                    <div class="card-body">
-                        <form action="#" method="post">
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+    />
+    <link type="text/css" href="/assets/css/argon.min.css" rel="stylesheet" />
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://kit.fontawesome.com/7ae2cf8851.js"
+      crossorigin="anonymous"
+    ></script>
+  </head>
+  <body>
+    <section class="vh-100" style="background-color: #eee">
+      <div class="container h-100">
+        <div class="row d-flex justify-content-center align-items-center h-100">
+          <div class="col-lg-12 col-xl-11">
+            <div class="card text-black" style="border-radius: 25px">
+              <div class="card-body p-md-5">
+                <div class="row justify-content-center">
+                  <div class="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+                    <p class="text-center h1 fw-bold mb-5 mx-1 mx-md-4 mt-4">
+                      Sign up
+                    </p>
 
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="username">
-                                <i class="fas fa-user"></i>
-                            </span>
-                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="username">
+                    <form class="mx-1 mx-md-4">
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-user fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                          <input
+                            type="text"
+                            id="form3Example1c"
+                            class="form-control"
+                            placeholder="Username"
+                          />
                         </div>
-                        
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="password">
-                                <i class="fas fa-lock"></i>
-                            </span>
-                            <input type="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="password">
+                      </div>
+
+                      <div class="d-flex flex-row align-items-center mb-4">
+                        <i class="fas fa-lock fa-lg me-3 fa-fw"></i>
+                        <div class="form-outline flex-fill mb-0">
+                          <input
+                            type="password"
+                            id="form3Example4c"
+                            class="form-control"
+                            placeholder="Username"
+                          />
                         </div>
-                        <div class="text-center d-grid gap-2 mt-4 mb-4">
-                            <button type="submit" name="" class="btn btn-primary mb-4">Sign in</button>
-                            <p>Not registered yet? <a href="#">Register</a></p>
-                        </div>
-                        </form>
-                    </div>
+                      </div>
+
+                      <div
+                        class="d-flex justify-content-center mx-4 mb-3 mb-lg-4"
+                      >
+                        <button type="button" class="btn btn-primary btn-lg">
+                          Register
+                        </button>
+                      </div>
+                    </form>
+                  </div>
+                  <div
+                    class="
+                      col-md-10 col-lg-6 col-xl-7
+                      d-flex
+                      align-items-center
+                      order-1 order-lg-2
+                    "
+                  >
+                    <img
+                      src="image/logo.png"
+                      class="img-fluid"
+                      alt="Sample image"
+                    />
+                  </div>
                 </div>
+              </div>
             </div>
-            <div class="col-md-4"></div>
+          </div>
         </div>
-    </div>
-    
-</body>
+      </div>
+    </section>
+  </body>
 </html>
