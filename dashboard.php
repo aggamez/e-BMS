5b80576030c882c32bda393c79910d773d6c93f8
dadelosreyes <DELOSREYES.DAVE@UE.EDU.PH>
2021-12-14 00:51:47 +0800

    ewan ko  na

--- dashboard.php
+++ dashboard.php
@@ -6,15 +6,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.0/font/bootstrap-icons.css">
-    <link rel="stylesheet" href="css/styles.css"><link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <link rel="stylesheet" href="css/dashboard.css">
 
-    <script src="scripts/js/bootstrap.bundle.min.js"></script>
+    <script src="scripts/js/bootstr ap.bundle.min.js"></script>
 </head>
 <body>
     <!-- navbar -->
-
-
     <nav class="navbar navbar-dark bg-dark">
       <div class="container-fluid">
         <div class="navbar-brand">
@@ -150,6 +149,10 @@
           </ul>
       </div>
     </div>
+    
+    
+
+    
 
     <!-- sidebar -->
 
